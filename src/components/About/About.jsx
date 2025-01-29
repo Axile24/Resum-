a0b9d4c6d8e575a4efff00329baec2f12f44c733
Axile24 <amokrane.n@icloud.com>
2025-01-30 00:10:50 +0100

    pushing från andra datorn

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,41 +3,41 @@ import { getImageUrl } from '../../utils'
 import styles from "./About.module.css"
 
 export const About = () => {
-  return (
+    return (
         <section className={styles.container} id='about'>
-         <h2 className={styles.title}> About</h2>
-         <div className={styles.content}>
+            <h2 className={styles.title}> WHO AM I?</h2>
+            <div className={styles.content}>
 
-            <img src={getImageUrl("about/aboutImage.png")} alt='me stitting with laptop'className={styles.aboutImage}/>
+                <img src={getImageUrl("about/aboutImage.png")} alt='me stitting with laptop' className={styles.aboutImage} />
 
-            <ul className={styles.aboutItemes}>
+                <ul className={styles.aboutItemes}>
 
-                <li className={styles.aboutItem}>
-                <img src={getImageUrl("about/cursorIcon.png")} alt='Cursor Icon'/>
-                <div className={styles.aboutItemText}>
-                    <h3>Frontend Developer</h3>
-                    <p>I'm a frondend developer with experience in responsive and optimized sites</p>
-                </div>
-                
-                </li>
-                <li className={styles.aboutItem}>
-                    <img src={getImageUrl("about/serverIcon.png")} alt='Server Icon'/>
-                <div className={styles.aboutItemText}>
-                    <h3>Backend Developer</h3>
-                    <p> I have an experince in developing fast and optimised backend systems and API's</p>
-                </div>
-                </li >
+                    <li className={styles.aboutItem}>
+                        <img src={getImageUrl("about/cursorIcon.png")} alt='Cursor Icon' />
+                        <div className={styles.aboutItemText}>
+                            <h3>Frontend Developer</h3>
+                            <p>I'm a frondend developer with experience in responsive and optimized sites</p>
+                        </div>
 
-                <li className={styles.aboutItem}><img src={getImageUrl("about/cursorIcon.png")} alt='xray Icon'/>
-                <div className={styles.aboutItemText}>
-                    <h3>X- ray technologist</h3>
-                    <p>I am an <b> X-ray Technologist</b>, a healthcare professional specializing in diagnostic imaging. I operate X-ray equipment to capture images of the body, helping doctors diagnose and treat medical conditions. My role involves ensuring patient safety, maintaining imaging equipment, and collaborating with healthcare teams to provide accurate results. I am passionate about delivering high-quality care and supporting patients through their medical journeys.</p>
-                </div>
-                
-                </li>
-            </ul>
-         </div>
+                    </li>
+                    <li className={styles.aboutItem}>
+                        <img src={getImageUrl("about/serverIcon.png")} alt='Server Icon' />
+                        <div className={styles.aboutItemText}>
+                            <h3>Backend Developer</h3>
+                            <p> I have an experince in developing fast and optimised backend systems and API's</p>
+                        </div>
+                    </li >
+
+                    <li className={styles.aboutItem}><img src={getImageUrl("about/cursorIcon.png")} alt='xray Icon' />
+                        <div className={styles.aboutItemText}>
+                            <h3>X- ray technologist</h3>
+                            <p>I am an <b> X-ray Technologist</b>, a healthcare professional specializing in diagnostic imaging. I operate X-ray equipment to capture images of the body, helping doctors diagnose and treat medical conditions. My role involves ensuring patient safety, maintaining imaging equipment, and collaborating with healthcare teams to provide accurate results. I am passionate about delivering high-quality care and supporting patients through their medical journeys.</p>
+                        </div>
+
+                    </li>
+                </ul>
+            </div>
 
         </section>
-  )
+    )
 }
--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,39 +1,44 @@
-import React from 'react'
-import Skills from "../../data/Skills.json"
-import { getImageUrl } from '../../utils'
+import React from "react";
+import Skills from "../../data/Skills.json";
+import { getImageUrl } from "../../utils";
+import history from "../../data/History.json";
+import styles from "./Experience.module.css";
 
 export default function Experience() {
   return (
-   <section id='experience'>
-    <h2>Experience</h2>
-      <div>
-   <div>
-    {
-        Skills.map((Skill, id)=> {
-        return (
-        <div key={id}>
-            <div>
-            <img src={getImageUrl(Skill.imageSrc)} alt={Skill.title} />
-
+    <section className={styles.container} id="experience">
+      <h2 className={styles.title}>Experience</h2>
+      <div className={styles.content}>
+        {/* Skills Section */}
+        <div className={styles.skills}>
+          {Skills.map(({ imageSrc, title }, id) => (
+            <div key={id} className={styles.skill}>
+              <div className={styles.imageContainer}>
+                <img src={getImageUrl(imageSrc)} alt={title} />
+              </div>
+              <p>{title}</p>
             </div>
-            <p> {Skill.title}</p>
+          ))}
         </div>
-        );
-        })
-    }
-    
-    </div>
-      <div></div>
 
-
-
-   </div>
-   
-   
-   
-   </section>
-
-
-
-  )
+        {/* Experience History */}
+        <ul className={styles.history}>
+          {history.map(({ imageSrc, organisation, role, startDate, endDate, experiences }, id) => (
+            <li key={id} className={styles.historyItem}>
+              <img src={getImageUrl(imageSrc)} alt={`${organisation} Logo`} />
+              <div className={styles.historyItemDetails}>
+                <h3>{`${role}, ${organisation}`}</h3>
+                <p>{`${startDate} - ${endDate}`}</p>
+                <ul>
+                  {experiences.map((experience, index) => (
+                    <li key={index}>{experience}</li>
+                  ))}
+                </ul>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
 }
