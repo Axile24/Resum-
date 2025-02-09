7b844aa6fcdf88152a66e7b5bac3530269f050ff
Axile24 <amokrane.n@icloud.com>
2025-02-09 03:30:31 +0100

    lagt till contact footer section

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { getImageUrl } from '../../utils'
+import styles from "./Contact.module.css"
+
+export  const Contact =()=> {
+  return <footer id='contact' className={styles.container}>
+    <div className='styles.text'><h2>Contact</h2><p> Free to reach out</p> </div>
+
+    <ul className={styles.links}>
+      <li className={styles.link}>
+       <img src={ getImageUrl('Contact/emailIcon.png')}alt='Email Icon'/>
+       <a href="mailto:amokrane.n@icloud.com">amokrane.n@icloud.com</a>
+      </li>
+
+
+      <li className={styles.link}>
+       <img src={ getImageUrl('Contact/linkedinIcon.png')}alt='linked Icon'/>
+       <a href="https://www.linkedin.com/myname">linkedin.com/myname</a>
+      </li>
+
+      <li className={styles.link}>
+       <img src={ getImageUrl('Contact/githubIcon.png')}alt='github Icon'/>
+       <a href="https://www.github.com/myname">github.com/axile</a>
+      </li>
+
+
+
+    </ul>
+    
+     </footer>
+}
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,10 +7,9 @@ export default function Hero() {
     <section className={styles.container}>
       <div className={styles.content}>
         <h1 className={styles.title}> Hi, I'm Nadjib</h1>
-        <p className={styles.description}>I'm full stack developper with 5 
-        years of experience using Java,  Java Entreprise,  JavaScript, 
-        DevOps, Mysql, Postgressql, laravels React and Node Js.
-        Reach out if you'd like more</p>
+        <p className={styles.description}>
+        Jag har en bakgrund inom både medicinsk bilddiagnostik och IT. Jag är utbildad röntgensjuksköterska med erfarenhet av olika modaliteter och har även kompetens inom Java Enterprise, JavaScript och DevOps. Min kombination av medicinsk och teknisk expertis gör att jag kan bidra inom både radiologi och systemutveckling.
+        </p>
         <a href="mailto:amokrane.n@icloud.com" className={styles.contactBtn}>Contact me</a>
       </div>to
       <img src={getImageUrl('hero/heroImage.jpg')} alt='Hero image of me' className={styles.heroImg} />
